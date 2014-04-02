9a1efd88803c6b20babd1324255e90c7ae43a662
Longi Joonas K <jlongi@da3-cs-dk110-07.helsinki.fi>
2014-04-02 18:36:04 +0300

    ui

--- ohtuminiprojekti/src/main/java/com/mycompany/ohtuminiprojekti/App.java
+++ ohtuminiprojekti/src/main/java/com/mycompany/ohtuminiprojekti/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        UI ui = new UI();
+        ui.addKirja();
     }
 }
---
+++ ohtuminiprojekti/src/main/java/com/mycompany/ohtuminiprojekti/UI.java
@@ -0,0 +1,40 @@
+
+package com.mycompany.ohtuminiprojekti;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UI {
+
+    private Scanner scanner;
+    private BookWriter bookwriter;
+    
+    public UI() {    
+        scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
+        try{
+            bookwriter = new BookWriter(list);
+        } catch (Exception e){System.out.println("wrtitteria ei luotu" + e);}
+    }
+    
+    public void addKirja(){
+        System.out.println("Anna Author");
+        //String author = scanner.nextLine();
+        System.out.println("Anna Title");
+        //String title = scanner.nextLine();
+        System.out.println("Anna Year");
+        //String year = scanner.nextLine();
+        System.out.println("Anna Publisher");
+        //String publisher = scanner.nextLine();
+        //System.out.println("tallennetaanko? (kyllä, ei)");
+        String info[] = {"kova koodatut authorit moi", "title", "vuosi", "publisher"};
+        
+        try {
+            bookwriter.write(info);
+        } catch (Exception e) {
+            System.out.println("ei onnaa " + e);
+        }
+    }
+
+}
