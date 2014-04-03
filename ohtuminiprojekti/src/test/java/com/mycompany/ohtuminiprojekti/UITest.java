e4fda1398a6e42c6973d1cf20125ace9988d8ce9
Pavel Smirnov <pavelsmirnov94@gmail.com>
2014-04-03 11:59:52 +0300

    pöö

--- ohtuminiprojekti/src/test/java/com/mycompany/ohtuminiprojekti/UITest.java
+++ ohtuminiprojekti/src/test/java/com/mycompany/ohtuminiprojekti/UITest.java
@@ -38,9 +38,8 @@ public class UITest {
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    //@Test
+    public void hello()
+    {
+    }
 }
